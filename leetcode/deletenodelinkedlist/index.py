70a2a3f0e878b0b0bf613d245cf0e818e6941005
rpraval1 <rpraval1@binghamton.edu>
2017-04-10 16:47:17 -0400

    delete node from linked list

---
+++ leetcode/deletenodelinkedlist/index.py
@@ -0,0 +1,24 @@
+class ListNode(object):
+	def __init__(self,x):
+		self.val = x
+		self.next = None
+	def __repr__(self):
+		if self:
+			return "{}->{}".format(self.val,repr(self.next))
+class Solution(object):
+	def deleteNode(self,head,node):
+		"""
+		:type node: ListNode
+		"""
+		print(head)
+		if node.next:
+			node.val = node.next.val
+			node.next = node.next.next
+		return head
+
+head = ListNode(3)
+head.next = ListNode(5)
+head.next.next = ListNode(7)
+head.next.next.next = ListNode(10)
+solution = Solution()
+print(solution.deleteNode(head,head.next))
