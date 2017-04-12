6efa98e1f83ec1d1c016cd70315175988434f25e
rpraval1 <rpraval1@binghamton.edu>
2017-04-12 17:22:46 -0400

    Merge Two Sorted Arrays

--- leetcode/arrays/mergeSortedArray/index.py
+++ leetcode/arrays/mergeSortedArray/index.py
@@ -1,6 +1,5 @@
 """
-:Author: Pravallika
-:Description: Given two sorted integer arrays num1 and num2, merge nums2 into nums1 as one sorted array.
+:Author: Pravallika:Description: Given two sorted integer arrays num1 and num2, merge nums2 into nums1 as one sorted array.
 :Example: nums1 = [2,4,6,7,9] nums2 = [1,3,6,8] resulted nums1 = [1,2,3,4,6,6,7,8,9]
 """
 class MergeSorted(object):
@@ -12,25 +11,18 @@ class MergeSorted(object):
 		:type n: int
 		:rtype: void
 		"""
-		i = 0 
-		j = 0
 
-		while i<m and j<n:
-			print(i,j)
-			if nums1[i] > nums2[j]:
+		while m>0 and n>0:
+			if nums1[m-1] >= nums2[n-1]:
 				
-				nums1.append(nums2[j])
-				if j == n-1 and i == m-1:
-					nums1.append(nums1[i])
-				j = j+1
+				nums1[m+n-1] = nums1[m-1]
+				m = m-1
 			else:
-				nums1.append(nums1[i])
-				if i == m-1 and j == n-1:
-					nums1.append(nums2[j])
-				i = i+1
-
-		for i in range(m):
-			nums1.pop(0)
+				nums1[m+n-1] = nums2[n-1]
+				n = n-1
+		if n > 0:
+			for i in range(n):
+				nums1[i] = nums2[i]
 		
 		print(nums1)	
 
