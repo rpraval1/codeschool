33964e248c8bd968efd7905742d053ed2b21e69f
rpraval1 <rpraval1@binghamton.edu>
2017-04-12 11:00:51 -0400

    Minimum Window Substring

--- leetcode/hashtable/minWindowSubstring/index.py
+++ leetcode/hashtable/minWindowSubstring/index.py
@@ -18,17 +18,27 @@ class MinWindow(object):
 		
 		tempFreq = {}
 		ptr1 = 0
-		
+		result = ''
+
 		for i,c in enumerate(s):
 			if c in tFreq:
 				tempFreq[c] = tempFreq.get(c,0)+1
+				if tempFreq[c] > tFreq[c]:
+					ptr += 1
+					i = ptr1
+					tempFreq = {}
 			print(tempFreq)
 			if tempFreq == tFreq:
 				print(tempFreq)
+				if result == '':
+					result = s[ptr1:i+1]
+				elif len(result) > len(s[ptr1:i+1]):
+					result = s[ptr1:i+1]
 				ptr1 = ptr1 + 1
 				i = ptr1
 				tempFreq = {}
-
+		print(result)
+		
 minWindow = MinWindow()
 minWindow.minWindowSubstring("ADOBECODEBANC","ABC")
 	
