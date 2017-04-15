a6dcd6443f642f621ff5a8a9fecd75dabaed24eb
rpraval1 <rpraval1@binghamton.edu>
2017-04-15 00:30:13 -0400

    ReactJS simple component

--- ReactJS/concepts/simpleComponent.jsx
+++ ReactJS/concepts/simpleComponent.jsx
@@ -3,12 +3,12 @@
 :Description: A simple component in ReactJS
 */}
 
-   import React from 'react';
+import React from 'react';
 
-   export default class TodoBox extends React.Component{
-      render() {
-        return <div className="todoBox">
-            Hello, world!
-          </div>
-      }
-   }
+export default class TodoBox extends React.Component{
+  render() {
+    return <div className="todoBox">
+        Hello, world!
+      </div>
+    }
+ }
