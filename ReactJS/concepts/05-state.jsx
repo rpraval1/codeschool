64c42aeca99732d450ea3a7fe83df687ef43bfd1
rpraval1 <rpraval1@binghamton.edu>
2017-05-12 15:46:33 -0400

    Use of state

---
+++ ReactJS/concepts/05-state.jsx
@@ -0,0 +1,78 @@
+{/*
+:Author: Pravallika
+:Description: Usage of props: pass values from a parent component to a child component.
+              A child component can have values handed to it either through attributes, or
+              through nested content.
+
+*/}
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class TodoBox extends React.Component {
+    render() {
+        return (
+            <div className="todoBox">
+                <h1>Todos</h1>
+                <TodoList />
+                <TodoForm />
+            </div>
+        );
+    }
+}
+
+class TodoList extends React.Component {
+
+  render(){
+    return (
+      <div className="todoList">
+        <table style={{border: "2px solid black"}}>
+            <tbody>
+            <Todo title="Shopping">Milk</Todo>
+            <Todo title="Hair cut">13:00</Todo>
+            <Todo title="Learn React">15:00</Todo>
+            </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+class Todo extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      checked: false
+    }
+  }
+
+  handleChange(){
+    this.setState({
+      checked: true
+    })
+  }
+  render(){
+    const {title} = this.props
+    return (
+      <tr>
+        <td style={{border: "1px solid black"}}>
+          <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)} />
+        </td>
+        <td style={{border:"1px solid black"}}>{title}</td>
+        <td style={{border:"1px solid black"}}>{this.props.children}</td>
+      </tr>
+    )
+  }
+}
+
+Todo.propTypes = {
+  title: PropTypes.string.isRequired
+};
+class TodoForm extends React.Component {
+
+  render(){
+    return (
+      <div className="todoForm">I am a TodoForm.</div>
+    )
+  }
+}
